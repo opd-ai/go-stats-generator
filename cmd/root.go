f7d7b72cf927da240610b53066ff0ab7ba0b31d0
user <user@mail.com>
2026-03-02 22:14:27 -0500

    docs(cmd): improve Execute function comment clarity

--- cmd/root.go
+++ cmd/root.go
@@ -41,7 +41,7 @@ Performance:
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
+// It is called by main.main() and only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
